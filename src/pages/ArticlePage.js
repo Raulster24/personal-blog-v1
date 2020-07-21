67fea820f9069621b8480b7d661048edc1989bad
Rahul Srivastava <rahulsri.pccs@gmail.com>
2020-07-21 13:48:23 +0400

    [fix] : Added 404 page and CSS Styling

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,15 +4,20 @@
 /* eslint-disable prefer-destructuring */
 import React from 'react';
 import articleContent from './article-content';
+import ArticlesList from '../components/articlesList';
+import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find((artic) => artic.name === name);
-  if (!article) return <h1>Article does not exist!</h1>;
+  if (!article) return <NotFoundPage />;
+  const otherArticles = articleContent.filter((articl) => articl.name !== name);
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => <p key={key}>{paragraph}</p>)}
+      <h3>Other Articles</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
