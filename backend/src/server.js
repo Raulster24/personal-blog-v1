bbbd5178718089eeb8bacc491959284ff21de8c9
Rahul Srivastava <rahulsri.pccs@gmail.com>
2020-07-21 15:35:57 +0400

    [feat] : added express js and defined get and post request for comments and upvotes

---
+++ backend/src/server.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+    comments: [],
+  },
+  'learn-node': {
+    upvotes: 0,
+    comments: [],
+  },
+  'learn-angular': {
+    upvotes: 0,
+    comments: [],
+  },
+};
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const articleName = req.params.name;
+  const { username, text } = req.body;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
+
+app.listen(8000, () => console.log('app is listening on port 8000'));
